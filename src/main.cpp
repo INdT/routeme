d1779d45f62ad82387f66f50f79d7a786fe6b076
André Loureiro <andre.loureiro@indt.org.br>
2012-08-10 15:13:08 -0400

    Updated main file to call RouteMe class
    Signed-off-by: André Loureiro <andre.loureiro@indt.org.br>

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <QtGui/QApplication>
 
+#include "routeme.h"
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     app.setApplicationName("RouteMe");
     app.setOrganizationName("INdT");
+
+    RouteMe routeme;
+    routeme.setGeometry(0, 0, 480, 864);
+
+#if defined(MEEGO_EDITION_HARMATTAN)
+    routeme.showFullScreen();
+#else
+    routeme.show();
+#endif
 
     return app.exec();
 }
